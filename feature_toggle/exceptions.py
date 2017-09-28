6af3ac0387d2012ad2d9f2da7c5bb94b58e3b039
Thulasiram <thulasi@innoventes.co>
2017-09-28 16:38:07 +0530

    Added custom exceptions

---
+++ feature_toggle/exceptions.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+
+class FeatureToggleDoesNotExist(Exception):
+    """
+    This exception is to aid consumers in providing an common exception to catch instead of relying on
+    model.DoesNotExist.
+    """
+
+    def __init__(self, **kwargs):
+        msg = 'FeatureToggle cannot be created with data: {d}'.format(d=kwargs)
+        super(FeatureToggleDoesNotExist, self).__init__(self, msg)
+
+
+class FeatureToggleAlreadyExists(Exception):
+    """
+    This exception is raised at multiple places.
+    Eg: when trying to create a toggle that already exists.
+    """
+
+    def __init__(self, **kwargs):
+        msg = kwargs.pop('msg')
+        if not msg:
+            msg = 'FeatureToggle already exists'
+            if kwargs:
+                msg += 'for data: {d}'.format(d=kwargs)
+        super(FeatureToggleAlreadyExists, self).__init__(self, msg)
+
+
+class FeatureToggleAttributeDoesNotExist(Exception):
+    """
+    This exception is to aid consumers in providing an common exception to catch instead of relying on
+    model.DoesNotExist for attributes
+    """
+
+    def __init__(self, **kwargs):
+        msg = 'FeatureToggleAttribute cannot be created with data: {d}'.format(d=kwargs)
+        super(FeatureToggleAttributeDoesNotExist, self).__init__(self, msg)
+
+
+class FeatureToggleAttributeAlreadyExists(Exception):
+    """
+    Wen trying to create a toggle attribute that already exists.
+    """
+
+    def __init__(self, **kwargs):
+        msg = 'FeatureToggleAttribute already exists with data: {d}'.format(d=kwargs)
+        super(FeatureToggleAttributeAlreadyExists, self).__init__(self, msg)
