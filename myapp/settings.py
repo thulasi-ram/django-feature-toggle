04ca738842f7ff714743f181f845562aafd79e26
Thulasi Ram <thulasi503@gmail.com>
2018-02-17 20:37:18 +0530

    Added auth backends

--- myapp/settings.py
+++ myapp/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = '5*$2anb4lgw(d3c7p(_=xp=hb7o76%2qp=g^re=#ei50zzu44w'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -69,7 +67,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'myapp.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
@@ -79,7 +76,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -99,7 +95,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
@@ -113,11 +108,12 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
 
 INSTALLED_APPS = INSTALLED_APPS + ['feature_toggle']
-MIDDLEWARE_CLASSES = MIDDLEWARE # backwards compatability
+MIDDLEWARE_CLASSES = MIDDLEWARE  # backwards compatibility
+
+AUTHENTICATION_BACKENDS = ('django.contrib.auth.backends.ModelBackend',)
